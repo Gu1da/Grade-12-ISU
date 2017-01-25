51bda2548aeedf09a5aee489ab84f2b96c00aae4
Gu1da <nicholasguida.99@gmail.com>
2017-01-25 11:20:55 -0500

    Add files via upload

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,7 @@
  * Nicholas Guida
  * Jan 23
  *
- */
+ **/
  
  
 import java.util.Scanner;
@@ -10,19 +10,11 @@ import java.util.Scanner;
 public class Menu {
 	Scanner input = new Scanner(System.in);
 	
-	 public void clearScreen(){
+    public void clearScreen(){
     	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
     
-    //May not need
-    public boolean checkValid(int num, int low, int high){
-    	if(num >= low && num <= high)
-    		return true;
-    	else
-    		return false;
-    }
-    
-    public int getChoice(int max){
+	public int getChoice(int max){
     	int choice;
     	
     	do{
@@ -34,74 +26,5 @@ public class Menu {
   		}while(!(choice >= 1 && choice <= max));
   		
   		return choice;
-    }
-	
-  	public void mainMenu(){
-  		int choice;
-  		this.clearScreen();
-  		
-  		//output menu
-  		System.out.println("=========================================");
-  		System.out.println("              ~Main Menu~                ");
-  		System.out.println("-----------------------------------------");
-  		System.out.println("1: Create component  4: Manage components");
-  		System.out.println("2: Create memory     5: Manage memory    ");
-  		System.out.println("3: View memory       6: Exit             ");
-  		System.out.println("=========================================");
-  		
-  		//get choice
-  		choice = getChoice(1);
-  			
-  		//move on
-		if(choice == 1)
-			createComponent();
-  	}
-    
-    public void createComponentMenu(){
-    	int choice;
-    	clearScreen();
-    	
-    	System.out.println("=========================================");
-    	System.out.println("         ~Create a component~");
-    	System.out.println("-----------------------------------------");
-    	System.out.println("1: Create just component");
-    	System.out.println("2: Create component and create memory with it");
-    	System.out.println("=========================================");
-    	
-    	//get choice and check
-  		choice = getChoice(1);
-  		
-  		//move on	
-  	//	if(choice == 1)
-  	
-    }
-    
-    
-    
-    public void createComponent(){
-    	String compName;
-		String compBrand;
-		double compSpeed;			//In Mhz or Ghz, depending on the memory type
-		int compMaxSize; 				
-		char compType;				//Hdd, Cpu, Gpu, Ram, Ssd
-    	
-    	System.out.print("Name: ");
-    	compName = input.nextLine();
-    	
-    	System.out.print("Brand: ");
-    	compBrand = input.nextLine();
-    	
-    	System.out.print("Max memory size: ");
-    	compMaxSize = input.nextInt();
-    	
-    	System.out.print("Type of component");
-    	compType = input.next(".").charAt(0);
-    	
-    	System.out.print("Clock Speed: ");
-    	compSpeed = input.nextDouble();
-    	
-    	
-    	
-		cBD.addComponent(compName, compBrand, compSpeed, compMaxSize, compType);
     }
 }
--- src/MyComputer.java
+++ src/MyComputer.java
@@ -9,13 +9,71 @@
 import java.util.Scanner;
  
 public class MyComputer {
-    Scanner input = new Scanner(System.in);
-    ComponentDB cDB = new ComponentDB();
-    
+
     public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
     	
+    	ComponentDB cDB = new ComponentDB();
+    	System.out.println("Take the L");
     	Menu mu = new Menu();
-    	mu.mainMenu();
     	
+    	int choice;
+  		mu.clearScreen();
+  		
+  		//output menu
+  		System.out.println("=========================================");
+  		System.out.println("              ~Main Menu~                ");
+  		System.out.println("-----------------------------------------");
+  		System.out.println("1: Create component  4: Manage components");
+  		System.out.println("2: Create memory     5: Manage memory    ");
+  		System.out.println("3: View memory       6: Exit             ");
+  		System.out.println("=========================================");
+  		
+  		//get choice
+  		choice = mu.getChoice(1);
+  			
+  		//move on
+		if(choice == 1){
+    		mu.clearScreen();
+    	
+	    	System.out.println("=========================================");
+	    	System.out.println("         ~Create a component~");
+	    	System.out.println("-----------------------------------------");
+	    	System.out.println("1: Create just component");
+	    	System.out.println("2: Create component and create memory with it");
+	    	System.out.println("=========================================");
+	    	
+	    	//get choice and check
+	  		choice = mu.getChoice(1);
+	  		
+	  		//move on	
+	  		if(choice == 1){
+	  			String compName;
+				String compBrand;
+				double compSpeed;			//In Mhz or Ghz, depending on the memory type
+				int compMaxSize; 				
+				char compType;				//Hdd, Cpu, Gpu, Ram, Ssd
+		    	
+		    	System.out.print("Name: ");
+		    	compName = input.nextLine();
+		    	
+		    	System.out.print("Brand: ");
+		    	compBrand = input.nextLine();
+		    	
+		    	System.out.print("Max memory size: ");
+		    	compMaxSize = input.nextInt();
+		    	
+		    	System.out.print("Type of component");
+		    	compType = input.next(".").charAt(0);
+		    	
+		    	System.out.print("Clock Speed: ");
+		    	compSpeed = input.nextDouble();
+		    	
+		    	
+		    	
+				cDB.addComponent(compName, compBrand, compSpeed, compMaxSize, compType);
+	  		}
+		}		
     }
 }
+
